c2afa63fae8cb93a0ec35b78daed79c2e3f22e74
JenniD5 <elizabethduran0105@gmail.com>
2019-10-30 09:49:52 -0700

    shader(compile shader)

--- helloworld.js
+++ helloworld.js
@@ -1,4 +1,32 @@
 const canvas=document.getElementById("glcanvas");
+
+const loadShader = (gl, type, source)=>
+{
+  const shader=gl.createShader(type);
+  gl.shaderSource (shader, source)
+            //variable que declaramos arriba el tipo, segundo viene del tecer nombre de loasd shader
+  //para compilar, porque si no no sabermos si funciona o no 
+  gl.compileShader(shader);
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    return null;
+  } //si falla que se borre el shader, siempre tine que ir en la compilaicon
+return shader;
+//si no, regresa el shader ya dicho
+  
+
+}
+
+//para inicializar el shader
+const initShader = (gl,vsSource, fsSource)=>{
+//cargar shaders
+
+
+}
+
+
+
 const main=()=>{
 const gl=canvas.getContext("webgl");
 
@@ -6,9 +34,36 @@ if (!gl) {
     alert("Impossible d'initialiser WebGL. Votre navigateur ou votre machine peut ne pas le supporter.");
     return;
   }
-
+initShader(gl, vsSource, vsSource)
 gl.clearColor(0,0,0,1);//el color con el que va estar limpiando
 gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
 window.onload=main;
+
+// Vertex shader program
+
+const vsSource = `         
+attribute vec4 aVertexPosition;
+
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+
+void main() {
+  gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;  
+  
+}
+`;
+
+//fragment
+
+const fsSource = `
+    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+  `;
+
+
+  
+
+
